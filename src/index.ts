c7f62e0d642a9f7ab7454a8dbf4e8840ec5b957d
linconl-cmd <linconl.lee@mail.com>
2024-09-17 21:24:26 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-"use strict";
-console.log('hello world');
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 9999;
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) =>{
+    const { message } = req.body
+
+    if(!message) return res.status(400).send({error: "Parametro errado (Primeira validação/teste do gatão)"})
+
+    res.send ({message})
+})
+
+
+app.listen(port, () =>{
+    console.log(`Server runing on port http://localhost:${port}`)
+})
+
